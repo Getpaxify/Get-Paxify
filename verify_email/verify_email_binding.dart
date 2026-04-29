13a01aa201f515be6c8ff81e04aefa2b2c65468c
Getpaxify <getpaxify@gmail.com>
2026-04-29 21:33:08 +0100

    Delete verify_email directory

--- verify_email/verify_email_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:paxify/modules/verify_email/verify_email_logic.dart';
-
-class VerifyEmailBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => VerifyEmailLogic());
-  }
-}
