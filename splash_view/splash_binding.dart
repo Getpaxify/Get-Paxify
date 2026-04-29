14227f0625efc10592ba834c5c76cbec375e5fc6
Getpaxify <getpaxify@gmail.com>
2026-04-29 21:31:59 +0100

    Delete splash_view directory

--- splash_view/splash_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:get/get_instance/get_instance.dart';
-
-class SplashBinding extends Bindings {
-  @override
-  void dependencies() {
-    // Get.lazyPut(() => SplashLogic());
-  }
-}
