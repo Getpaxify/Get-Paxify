db0107bfea9f167254d262b9dd6309ca99f86b49
Getpaxify <getpaxify@gmail.com>
2026-04-29 21:34:47 +0100

    Delete utils directory

--- utils/extension/widget_extensions.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension WidgetSpacing on num {
-  SizedBox get spacingW => SizedBox(width: this as double);
-  SizedBox get spacingH => SizedBox(height: this as double);
-}
--- utils/strings.dart
+++
@@ -1,6 +0,0 @@
-/// GetX Template Generator - fb.com/htngu.99
-///
-library;
-
-String home = 'Home';
-String appName = 'PlanetF';
