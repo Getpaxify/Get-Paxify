76d73ae071518b3247a0114142bce959ca784365
Getpaxify <getpaxify@gmail.com>
2026-04-29 21:30:21 +0100

    Delete home_therapy directory

--- home_therapy/plan_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:paxify/modules/home_therapy/home_logic.dart';
-
-class PlanBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => HomeLogic());
-  }
-}
