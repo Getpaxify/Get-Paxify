0f4ad58ee86a5f12ca82134fcd1eb20d41b428f5
Getpaxify <getpaxify@gmail.com>
2026-04-29 21:29:23 +0100

    Delete notification directory

--- notification/notification_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'notification_logic.dart';
-
-class NotificationBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<NotificationLogic>(() => NotificationLogic());
-  }
-}
