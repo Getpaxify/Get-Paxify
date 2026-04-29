02eaf250b963941186a2d92b347f3071bfd4b576
Getpaxify <getpaxify@gmail.com>
2026-04-29 21:31:20 +0100

    Delete setting directory

--- setting/setting_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:paxify/modules/setting/setting_logic.dart';
-
-class SettingBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => SettingLogic());
-  }
-}
