542d400cd5389418e9247b836f54d2096f31c7e3
Getpaxify <getpaxify@gmail.com>
2026-04-29 21:31:35 +0100

    Delete signup directory

--- signup/get_started_binding.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:get/get.dart';
-// import 'package:planetf/modules/signup/sign_up_logic.dart';
-
-// class GetStartedBinding extends Bindings {
-//   @override
-//   void dependencies() {
-//     Get.lazyPut(() => SignUpLogic());
-//   }
-// }
