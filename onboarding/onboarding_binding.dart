d651d2347c1de1305981c470a95d87c9f2d17aa1
Getpaxify <getpaxify@gmail.com>
2026-04-29 21:30:50 +0100

    Delete onboarding directory

--- onboarding/onboarding_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:paxify/modules/onboarding/onboarding_logic.dart';
-
-class OnboardingBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => OnboardingLogic());
-  }
-}
